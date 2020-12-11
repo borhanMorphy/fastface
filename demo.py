4572d0200f968aeafb00dee8ab3f30f311feb7c7
borhanMorphy <borhano.f.42@gmail.com>
2020-12-12 01:29:19 +0300

    .gitignore update

--- demo.py
+++ demo.py
@@ -19,30 +19,31 @@ def get_arguments():
     return ap.parse_args()
 
 def load_image(img_path:str):
-    return cv2.cvtColor(cv2.imread(img_path), cv2.COLOR_BGR2RGB)
+    return cv2.imread(img_path)
 
 def re_transform(batch:torch.Tensor):
     batch = (batch * 127.5) + 127.5
     img = batch.permute(1,2,0).cpu().numpy()
     img = img.astype(np.uint8)
-    return img[:,:,[2,1,0]]
+    return img
 
 def main(img_path:str, model_path:str, target_size:int=640):
     img = load_image(img_path)
     model = LightFaceDetector.from_pretrained("lffd", model_path)
 
     transforms = Compose(
-        Interpolate(max_dim=target_size),
-        Padding(target_size=(target_size,target_size), pad_value=0),
+        #Interpolate(max_dim=target_size),
+        #Padding(target_size=(target_size,target_size), pad_value=0),
         Normalize(mean=127.5, std=127.5),
         ToTensor()
     )
 
-    batch = transforms(img)
-
     model.eval()
 
+    batch = transforms(img)
+
     preds = model.predict(batch)[0]
+    print(preds)
     img = re_transform(batch)
 
     for pred in preds:
@@ -51,11 +52,10 @@ def main(img_path:str, model_path:str, target_size:int=640):
         print(x1,y1,x2,y2)
         score = pred[4]
         print(score)
-        timg = img.copy()
-        timg = cv2.rectangle(timg, (x1,y1),(x2,y2),(0,0,255),2)
-        cv2.imshow("",timg)
-        if cv2.waitKey(0) == 27:
-            break
+        img = cv2.rectangle(img, (x1,y1),(x2,y2),(0,0,255),2)
+    cv2.imshow("",img)
+    if cv2.waitKey(0) == 27:
+        pass
 
 
 if __name__ == '__main__':
