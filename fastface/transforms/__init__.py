4050e0785a4b94ff7bad1e3bc99fa303cdb372c3
borhanMorphy <borhano.f.42@gmail.com>
2021-06-01 12:23:13 +0300

    normalize removed from transforms

--- fastface/transforms/__init__.py
+++ fastface/transforms/__init__.py
@@ -2,5 +2,4 @@ from .augmentation import *
 from .interpolate import Interpolate, ConditionalInterpolate
 from .pad import Padding
 from .discard import FaceDiscarder
-from .normalize import Normalize
 from .compose import Compose
