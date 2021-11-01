fc66867001a0ad940ae7437b57f3b20d028589ff
borhanMorphy <borhano.f.42@gmail.com>
2021-11-01 00:26:31 +0000

    gdown and http progress bar added

--- fastface/adapter/gdrive.py
+++ fastface/adapter/gdrive.py
@@ -1,16 +1,17 @@
 import os
 
-from google_drive_downloader import GoogleDriveDownloader as gdd
+import gdown
 
+from .extract_handler import ExtractHandler
 
 class GoogleDriveAdapter:
+
     @staticmethod
     def download(
         dest_path: str,
         file_name: str = None,
         file_id: str = None,
         extract: bool = False,
-        showsize: bool = True,
         **kwargs
     ):
         assert isinstance(file_id, str), "file id must be string but found:{}".format(
@@ -22,6 +23,15 @@ class GoogleDriveAdapter:
         if not os.path.exists(dest_path):
             os.makedirs(dest_path, exist_ok=True)
         file_path = os.path.join(dest_path, file_name)
-        gdd.download_file_from_google_drive(
-            file_id, file_path, unzip=extract, showsize=showsize, **kwargs
+
+        gdown.download(
+            output=file_path,
+            quiet=False,
+            use_cookies=False,
+            id=file_id,
         )
+
+        if not extract:
+            return
+
+        ExtractHandler.extract(file_path, dest_path, **kwargs)
--- fastface/adapter/http.py
+++ fastface/adapter/http.py
@@ -1,11 +1,13 @@
 import os
 
 import requests
+from tqdm import tqdm
 
 from .extract_handler import ExtractHandler
 
 
 class HttpAdapter:
+
     @staticmethod
     def download(
         dest_path: str,
@@ -19,7 +21,7 @@ class HttpAdapter:
         assert isinstance(url, str), "url must be string but found:{}".format(type(url))
 
         file_name = url.split("/")[-1] if file_name is None else file_name
-        res = requests.get(url)
+        res = requests.get(url, stream=True)
 
         assert (
             res.status_code == 200
@@ -28,13 +30,23 @@ class HttpAdapter:
             res.status_code, res.content
         )
 
+        total = int(res.headers.get('content-length', 0))
+
         file_path = os.path.join(dest_path, file_name)
 
         if not os.path.exists(dest_path):
             os.makedirs(dest_path, exist_ok=True)
 
-        with open(file_path, "wb") as foo:
-            foo.write(res.content)
+        with open(file_path, 'wb') as foo, tqdm(
+            desc=file_path,
+            total=total,
+            unit='iB',
+            unit_scale=True,
+            unit_divisor=1024,
+        ) as bar:
+            for data in res.iter_content(chunk_size=1024):
+                size = foo.write(data)
+                bar.update(size)
 
         if not extract:
             return
